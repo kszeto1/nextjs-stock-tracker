3000a00e441e7292001342b5ace808099f9a7c4c
kszeto1 <kenny.szeto1@gmail.com>
2024-06-24 14:23:17 -0700

    Add navbar, dashboard route

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="navbar bg-white">
+      <div className="flex-1">
+        <a className="btn bg-primary text-xl text-black">Stock It Up</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
